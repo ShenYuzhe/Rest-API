25d01a2df1a42f4410fecc6db3114712e1fecd03
Trunksexy Shen <trunksexy@dyn-160-39-254-132.dyn.columbia.edu>
2015-09-21 12:39:37 -0400

    remove private infos

--- movieRest/routes/dbConfig.js
+++ movieRest/routes/dbConfig.js
@@ -1,18 +1,10 @@
-/*var mysql = require('mysql');
+var mysql = require('mysql');
 exports.conn = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'sakila',
     port: 3306
-});*/
-
-var mysql = require('mysql');
-exports.conn = mysql.createConnection({
-    host: 'spring2015db.casre98azkeg.us-west-2.rds.amazonaws.com',
-    user: 'shenyuzhe',
-    password: '19911126',
-    database: 'sakila',
-    port: 3306
 });
+
 
